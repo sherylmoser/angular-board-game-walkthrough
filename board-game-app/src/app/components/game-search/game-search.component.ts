17822da3aeae8f90bc8f40a7d3210fbf5bbbd1e6
sherylmoser <sherylrmoser@gmail.com>
2022-03-08 10:12:07 -0700

    part 8 of walkthrough complete

--- board-game-app/src/app/components/game-search/game-search.component.ts
+++ board-game-app/src/app/components/game-search/game-search.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { ListType } from 'src/app/enums/list-type';
+import { Game } from 'src/app/interfaces/game';
+import { GameService } from 'src/app/services/game.service';
+import { LocalStorageService } from 'src/app/services/local-storage.service';
+import * as _ from 'lodash';
 
 @Component({
   selector: 'app-game-search',
@@ -6,11 +11,51 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./game-search.component.scss']
 })
 export class GameSearchComponent implements OnInit {
-  searchText: string | undefined;
+  searchText: string;
+  games: Game[] = [];
+  ownedGames: { [id: string]: Game } = {};
+  wishListGames: { [id: string]: Game } = {};
 
-  constructor() { }
+  constructor(
+    private gameService: GameService,
+    private localStorageService: LocalStorageService
+  ) { }
 
   ngOnInit(): void {
+    this.localStorageService.getGameList(ListType.OWNEDLIST);
+    this.localStorageService.getGameList(ListType.WISHLIST);
+
+    this.localStorageService.ownedGames.subscribe(games => this.ownedGames = _.mapKeys(games, 'id'));
+    this.localStorageService.wishListGames.subscribe(games => this.wishListGames = _.mapKeys(games, 'id'))
+  }
+
+  search() {
+    this.gameService.searchByName(this.searchText).subscribe(games => this.games = games);
+  }
+  updateOwnedList(event: MouseEvent, game: Game) {
+    event.stopPropagation();
+
+    if (this.ownedGames[game.id]) {
+      this.localStorageService.deleteGame(game, ListType.OWNEDLIST);
+    } else {
+      if (this.wishListGames[game.id]) {
+        this.localStorageService.deleteGame(game, ListType.WISHLIST);
+      }
+      this.localStorageService.saveGame(game, ListType.OWNEDLIST);
+    }
+  }
+
+  updateWishlist(event: MouseEvent, game: Game) {
+    event.stopPropagation();
+
+    if (this.wishListGames[game.id]) {
+      this.localStorageService.deleteGame(game, ListType.WISHLIST);
+    } else {
+      if (this.ownedGames[game.id]) {
+        this.localStorageService.deleteGame(game, ListType.OWNEDLIST);
+      }
+      this.localStorageService.saveGame(game, ListType.WISHLIST);
+    }
   }
 
 }
--- board-game-app/src/app/modules/material.module.ts
+++ board-game-app/src/app/modules/material.module.ts
@@ -6,6 +6,9 @@ import { MatListModule } from '@angular/material/list';
 import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
 import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatCardModule } from '@angular/material/card';
+import { MatTooltipModule } from '@angular/material/tooltip';
 
 @NgModule({
   declarations: [],
@@ -16,7 +19,10 @@ import { MatFormFieldModule } from '@angular/material/form-field';
     MatListModule,
     MatIconModule,
     MatButtonModule,
-    MatFormFieldModule
+    MatFormFieldModule,
+    MatInputModule,
+    MatCardModule,
+    MatTooltipModule
   ],
   exports: [
     MatToolbarModule,
@@ -24,7 +30,10 @@ import { MatFormFieldModule } from '@angular/material/form-field';
     MatListModule,
     MatIconModule,
     MatButtonModule,
-    MatFormFieldModule
+    MatFormFieldModule,
+    MatInputModule,
+    MatCardModule,
+    MatTooltipModule
   ]
 })
 export class MaterialModule { }
