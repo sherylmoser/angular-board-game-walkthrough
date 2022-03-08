e7b6285857f38e56e28b723eae08a199762c5fe9
sherylmoser <sherylrmoser@gmail.com>
2022-03-08 08:06:28 -0700

    file system set-up

---
+++ board-game-app/src/app/app.module.ts
@@ -0,0 +1,32 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './modules/app-routing.module';
+import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MaterialModule } from './modules/material.module';
+import { SideNavComponent } from './components/side-nav/side-nav.component';
+import { GameSearchComponent } from './components/game-search/game-search.component';
+import { GameDetailsComponent } from './components/game-details/game-details.component';
+import { GameDetailControlsComponent } from './components/game-detail-controls/game-detail-controls.component';
+import { UserSelectionsComponent } from './components/user-selections/user-selections.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    SideNavComponent,
+    GameSearchComponent,
+    GameDetailsComponent,
+    GameDetailControlsComponent,
+    UserSelectionsComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MaterialModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
